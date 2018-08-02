13f383839bcfb14393de844ea1741a992f3ced41
ajinkya prabhu <ajinkyamprabhu1997@yahoo.com>
2018-08-02 18:36:31 +0530

    transfer learning model

--- Model.py
+++ Model.py
@@ -1,37 +1,39 @@
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense,Conv2D,MaxPooling2D,Dropout,Flatten,Activation,BatchNormalization
 
-
+from tensorflow.keras.applications import vgg19
 
 def create_model(n_out_classes = 32, dropout_enable = False):
 
-    # model = mobilenet.MobileNet(input_shape = (256,256,3),
-    #                             classes= n_out_classes,
-    #                             weights=None
-    #                             )
-    model = Sequential()
-    ##Conv2d Layer
-    model.add(Conv2D(64, (3, 3), activation='relu', input_shape=(256, 256, 3),padding="valid"))
-    model.add(Activation('relu'))
-    ##3x3 pooling
-    model.add(BatchNormalization())
-    model.add(MaxPooling2D(pool_size=(3, 3)))
-    model.add(Activation('relu'))
-    model.add(BatchNormalization())
-    model.add(Conv2D(32, (3, 3), activation='relu',padding="valid"))
-    model.add(Activation('relu'))
-    model.add(BatchNormalization())
-    model.add(MaxPooling2D(pool_size=(3, 3)))
-    model.add(Activation('relu'))
-    model.add(BatchNormalization())
+    # # model = mobilenet.MobileNet(input_shape = (256,256,3),
+    # #                             classes= n_out_classes,
+    # #                             weights=None
+    # #                             )
+    # model = Sequential()
+    # ##Conv2d Layer
+    # model.add(Conv2D(64, (3, 3), activation='relu', input_shape=(256, 256, 3),padding="valid"))
+    # model.add(Activation('relu'))
+    # ##3x3 pooling
+    # model.add(BatchNormalization())
+    # model.add(MaxPooling2D(pool_size=(3, 3)))
+    # model.add(Activation('relu'))
+    # model.add(BatchNormalization())
+    # model.add(Conv2D(32, (3, 3), activation='relu',padding="valid"))
+    # model.add(Activation('relu'))
+    # model.add(BatchNormalization())
+    # model.add(MaxPooling2D(pool_size=(3, 3)))
+    # model.add(Activation('relu'))
+    # model.add(BatchNormalization())
+    #
+    #
+    # if dropout_enable:
+    #     model.add(Dropout(0.20))
+    #
+    # model.add(Flatten())
+    #
+    # model.add(Dense(n_out_classes, activation='softmax'))
 
-    
-    if dropout_enable:
-        model.add(Dropout(0.20))
-
-    model.add(Flatten())
-
-    model.add(Dense(n_out_classes, activation='softmax'))
+    model = vgg19.VGG19(weights="./vgg19_weights_tf_dim_ordering_tf_kernels_notop.h5",include_top=False,classes=n_out_classes)
 
     print(model.summary())
 
--- train_model.py
+++ train_model.py
@@ -18,7 +18,7 @@ WIDTH,HEIGHT = 256,256
 train_data_dir = './crowdai/'
 N_CLASSES = 38
 nb_train_samples = 4125
-batch_size = 16
+batch_size = 8
 epochs = 10
 
 
@@ -26,7 +26,7 @@ epochs = 10
 model = create_model(N_CLASSES)
 
 model.compile(loss = "categorical_crossentropy",
-              optimizer = optimizers.Adam(lr=0.01),
+              optimizer = optimizers.Adam(lr=0.1),
               metrics =["accuracy"] )
 
 print(model.summary())
@@ -45,9 +45,9 @@ train_datagen = ImageDataGenerator()
 train_generator = train_datagen.flow_from_directory(train_data_dir,
                                                    target_size = (WIDTH, HEIGHT),
                                                    batch_size = batch_size,
-                                                   class_mode = "categorical")
+                                                   class_mode="categorical")
 checkpoint = ModelCheckpoint("leaf_checkpoint.h5",
-                             monitor='val_acc',
+                             monitor='val_loss',
                              verbose=1, save_best_only=True,
                              save_weights_only=False,
                              mode='auto', period=1)
@@ -58,11 +58,13 @@ board = TensorBoard(log_dir="./logs_mobilenet/",
                     batch_size=batch_size,
                     write_images=True,)
 
-
-
+try:
+    model.l("./plant_model1.h5")
+except:
+    print("Creating new Model....")
 model.fit_generator(train_generator,
                     epochs = epochs,
                     callbacks = [board,checkpoint])
 
 
-model.save("Whale_model_1.h5")
+model.save("leaf_model_trained.h5")
